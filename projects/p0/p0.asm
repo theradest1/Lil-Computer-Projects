1ffe830347315680b548285ad7aa8511b65eddbf
TheRad1 <18326@verona.k12.wi.us>
2023-10-10 10:11:56 -0500

    got mem corrupter done (:

--- projects/p0/p0.asm
+++ projects/p0/p0.asm
@@ -1,5 +1,8 @@
         .orig x3000
-START   ADD R2,R2,#1            ; R5 <= R2 + 1
-        HALT                    ; return to os
+START   LEA R2,#10				; load the start of corruption into R2
+LOOP	ADD R2,R2,#1            ; R2 <= R2 + 1
+		STR R2,R2,#0            ; set address in R2 to R2 (corrupt the mem yay)
+        JSR LOOP				; jump to start
+		HALT                    ; return to os
 	.end
         
