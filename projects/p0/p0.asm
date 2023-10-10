ceaf5aae7564d85950fa71ae0838ce22ee7cb20c
TheRad1 <18326@verona.k12.wi.us>
2023-10-10 15:29:30 -0500

    working on it (:

--- projects/p0/p0.asm
+++ projects/p0/p0.asm
@@ -1,8 +1,15 @@
-        .orig x3000
-START   LEA R2,#10				; load the start of corruption into R2
-LOOP	ADD R2,R2,#1            ; R2 <= R2 + 1
-		STR R2,R2,#0            ; set address in R2 to R2 (corrupt the mem yay)
-        JSR LOOP				; jump to start
+        .orig x2FFF				; 1 value earlier than usual for start mem
+		.FILL x0001				; put the inverted end value at this mem location
+		.FILL xC000				; start of screen mem
+START   LD R2,#-2				; load the start of screen into R2
+		LD R3,#-4				; load the inv-end of the screen into R3
+		ADD R4,R4,#5
+CORLOOP	ADD R2,R2,#1            ; R2 <= R2 + 1
+		ADD R4,R2,#1
+		STR R4,R2,#0            ; set address in R2 to R4 (corrupt the mem yay!)
+		ADD R3,R2,R3			; add current mem and inv-screen end to see if 
+		BRz START				; go to start if end of screen
+        JSR CORLOOP				; jump to loop
 		HALT                    ; return to os
 	.end
         
