404d3834b719db1f7489cabed2ae4e99a0758871
TheRad1 <18326@verona.k12.wi.us>
2023-10-11 17:24:47 -0500

    Update p1.asm

--- projects/p1/p1.asm
+++ projects/p1/p1.asm
@@ -5,15 +5,15 @@ START   LD R3,SCREEN_END			; load the end of the screen
 		NOT R3,R3				; change R3 to negative (pre-computing)
 		ADD R3,R3,#1
 
-INCOLOR LD R2,SCREEN_START			; load the start of screen into R2 (address counter)
+INCOLOR LD R5,SCREEN_START			; load the start of screen into R5 (address counter)
 
 LOOP	JSR RAND				; get random color
 		ADD R4,R1,#0
 
-		ADD R2,R2,#1            ; R2 <= R2 + 1
-		STR R4,R2,#0            ; mem[R2 + 0] <= R4
+		ADD R5,R5,#1            ; R5 <= R5 + 1
+		STR R4,R5,#0            ; mem[R5 + 0] <= R4
 
-		ADD R0,R2,R3			; R0 <= R2 - R3
+		ADD R0,R5,R3			; R0 <= R5 - R3
 		BRnp LOOP				; go to LOOP if previous calc is not zero
 
 		JSR INCOLOR				; if not end of screen mem, increment color and reset mem counter
@@ -30,14 +30,6 @@ RAND_SEED2 .FILL xE129
 		;;Functions ---------------
 
 RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R1
-		ADD R1,R1,R0
-		ADD R1,R1,R1
-		ADD R1,R1,R2
-		ADD R1,R1,R3
-		ADD R1,R1,R4
-		ADD R1,R1,R5
-		ADD R1,R1,R6
-		ADD R1,R1,R7
 		LD R0,RAND_SEED1
 		ADD R1,R1,R0
 		ST R1,RAND_SEED1
@@ -46,5 +38,12 @@ RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R1
 		ST R1,RAND_SEED2
 		RET
 
-		
+MULT	;multiplication - input R0, R1, output R2
+		AND R2,R2,#0
+MULTLOOP
+		ADD R0,R0,#-1
+		ADD R1,R1,R1
+		BRp MULTLOOP
+		RET
+	
 		.end
