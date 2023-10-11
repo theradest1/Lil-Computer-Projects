5bba503e25073f9bdd2d5495a7e6c57872079404
TheRad1 <18326@verona.k12.wi.us>
2023-10-11 18:08:13 -0500

    got mult done
    might have an entire multiplication part in the rand so I can use return

--- projects/p1/p1.asm
+++ projects/p1/p1.asm
@@ -1,49 +1,56 @@
         .orig x3000
-START   LD R3,SCREEN_END			; load the end of the screen
-		LD R1,COLOR_START
+START	LD R3,SCREEN_END		; change screen mem end to negative (pre-computing)
+		NOT R3,R3				; 
+		ADD R3,R3,#1			;
+		ST R3,SCREEN_END		;
 
-		NOT R3,R3				; change R3 to negative (pre-computing)
-		ADD R3,R3,#1
+INCOLOR LD R6,SCREEN_START			; load the start of screen into R5 (address counter)
 
-INCOLOR LD R5,SCREEN_START			; load the start of screen into R5 (address counter)
+LOOP	JSR RAND				; assign random color to mem[R6]
+EXIT	ADD R6,R6,#1            ;
+		STR R0,R6,#0            ;
 
-LOOP	JSR RAND				; get random color
-		ADD R4,R1,#0
+		LD R3,SCREEN_END		; check if at the end of the screen memory
+		ADD R0,R6,R3			;
+		BRnp LOOP				;
 
-		ADD R5,R5,#1            ; R5 <= R5 + 1
-		STR R4,R5,#0            ; mem[R5 + 0] <= R4
+		JSR INCOLOR				; increment color and reset mem counter
 
-		ADD R0,R5,R3			; R0 <= R5 - R3
-		BRnp LOOP				; go to LOOP if previous calc is not zero
 
-		JSR INCOLOR				; if not end of screen mem, increment color and reset mem counter
-		HALT					; return to os (never)
 
-		; data ----------------
+; data ----------------
 
-SCREEN_START .FILL xC000				; start of screen mem (+1 for example)
-SCREEN_END .FILL xFDFE				; End of screen mem (-1 for example)
-COLOR_START .FILL xFF00
-RAND_SEED1 .FILL xFAEF
-RAND_SEED2 .FILL xE129
+SCREEN_START .FILL xC000
+SCREEN_END .FILL xFDFE
+RAND_SEED .FILL xFAEF
+RAND_MULT .FILL xD298
 
-		;;Functions ---------------
 
-RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R1
-		LD R0,RAND_SEED1
-		ADD R1,R1,R0
-		ST R1,RAND_SEED1
-		LD R0,RAND_SEED2
-		ADD R1,R1,R0
-		ST R1,RAND_SEED2
-		RET
+;Functions ---------------
 
-MULT	;multiplication - input R0, R1, output R2
-		AND R2,R2,#0
-MULTLOOP
-		ADD R0,R0,#-1
-		ADD R1,R1,R1
-		BRp MULTLOOP
-		RET
+RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R0
+		LD R1,RAND_SEED
+		LD R2,RAND_MULT
+		JSR MULT
+		ST R0,RAND_SEED
+		JSR EXIT
+
+MULT	;multiplication - input R1, R2, output R0 - uses R0-R5
+		AND R0,R0,#0	; result
+		ADD R3,R0,#1	; bit test mask
+		ADD R4,R0,#-1	; end condition mask
+
+MULT1	AND R2,R2,R4	; any bits left
+		BRz MULT3			;
+		AND R5,R2,R3	; test bit
+		BRz MULT2			;
+		ADD R0,R0,R1	; add mult to result
+
+MULT2	ADD R1,R1,R1	; shift mult bits
+		ADD R3,R3,R3	;
+		ADD R4,R4,R4	;
+		BRnzp MULT1		; keep going
+
+MULT3	RET				; finished
 	
 		.end
