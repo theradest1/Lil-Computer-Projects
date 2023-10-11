930d185af5eb6ef34da07f8cf554c001ec5fbb2c
TheRad1 <18326@verona.k12.wi.us>
2023-10-11 11:28:39 -0500

    changed a few scripts and improved visuals

--- projects/p0/p0.asm
+++ projects/p0/p0.asm
@@ -9,14 +9,16 @@ START   LD R3,#-3				; load the end of the screen into R3
 		ADD R3,R3,#1
 
 INCOLOR ADD R4,R4,#1			; increment color
-		LD R2,#-9				; load the start of screen into R2
+		LD R2,#-9				; load the start of screen into R2 (address counter)
 
-LOOP	ADD R2,R2,#2            ; R2 <= R2 + 1		R2 is address coutnter
-		ADD R4,R4,#1			; R4 <= R2 + 1		R4 is color
+LOOP	ADD R2,R2,#1            ; R2 <= R2 + 1
+		ADD R4,R4,#1			; R4 <= R2 + 1
 		STR R4,R2,#0            ; mem[R2 + 0] <= R4
+
 		ADD R1,R2,R3			; R1 <= R2 - R3 R3
 		BRnp LOOP				; go to LOOP if previous calc is not zero
-		JSR INCOLOR
-		HALT
+
+		JSR INCOLOR				; if not end of screen mem, increment color and reset mem counter (R2)
+		HALT					; return to os (never)
 		.end
         
