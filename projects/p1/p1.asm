3c7ad9f3ebe63fe457482f3620dac38d35de1468
TheRad1 <18326@verona.k12.wi.us>
2023-10-12 00:27:15 -0500

    cleaned things up
    Idk why, but when I left the randomized colors to go up to only x7FFF, it looked the same as xFFFF

--- projects/p1/p1.asm
+++ projects/p1/p1.asm
@@ -12,7 +12,7 @@ START	LD R3,SCREEN_END		; change screen mem end to negative (pre-computing)
 INCOLOR LD R6,SCREEN_START			; load the start of screen into R5 (address counter)
 
 LOOP	JSR RAND				; assign random color to mem[R6]
-EXIT	ADD R6,R6,#1            ;
+		ADD R6,R6,#1            ;
 		STR R0,R6,#0            ;
 
 		LD R3,SCREEN_END		; check if at the end of the screen memory
@@ -29,17 +29,29 @@ SCREEN_START .FILL xC000
 SCREEN_END .FILL xFDFE
 
 RAND_SEED .FILL x0001
-RAND_MULT .FILL xF3B4
-RAND_INCR .FILL xA904
+RAND_MULT .FILL x0002
+RAND_INCR .FILL xD900
 RAND_LIM .FILL x7FFF		;(gets inverted)
 
 
 ;Functions ---------------
 
-RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R0
+RAND	; generates a random (ish) hex number from x0000 to x7FFF, stored in R0
 		LD R1,RAND_SEED		;
 		LD R2,RAND_MULT		; multiply seed with mult
-		JSR MULT			;
+		AND R0,R0,#0		; explinations are in mult subroutine
+		ADD R3,R0,#1		; 
+		ADD R4,R0,#-1		; 	
+RAND1	AND R2,R2,R4		;
+		BRz RAND3			;
+		AND R5,R2,R3		;
+		BRz RAND2			;
+		ADD R0,R0,R1		;
+RAND2	ADD R1,R1,R1		;	 
+		ADD R3,R3,R3		;
+		ADD R4,R4,R4		;
+		BRnzp RAND1			;
+RAND3						;
 
 		LD R1,RAND_INCR		; add increment
 		ADD R0,R0,R1		;
@@ -51,13 +63,13 @@ RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R0
 
 		ST R0,RAND_SEED		; store
 
-		JSR EXIT
+		RET
 
 		
 MULT	;multiplication - input R1, R2, output R0 - uses R0-R5
 		AND R0,R0,#0	; result
 		ADD R3,R0,#1	; bit test mask
-		ADD R4,R0,#-1	; end condition mask
+		ADD R4,R0,#-1	; end condition mask	
 
 MULT1	AND R2,R2,R4	; any bits left
 		BRz MULT3			;
