ccdf1ac366aa0de94fb1477cb4a426bc1843b012
TheRad1 <18326@verona.k12.wi.us>
2023-10-11 23:38:13 -0500

    getting closer!
    instead of making modulus, I am just doing a limit since that is the use case

--- projects/p1/p1.asm
+++ projects/p1/p1.asm
@@ -4,6 +4,11 @@ START	LD R3,SCREEN_END		; change screen mem end to negative (pre-computing)
 		ADD R3,R3,#1			;
 		ST R3,SCREEN_END		;
 
+		LD R3,RAND_LIM
+		NOT R3,R3
+		ADD R3,R3,#1
+		ST R3,RAND_LIM
+
 INCOLOR LD R6,SCREEN_START			; load the start of screen into R5 (address counter)
 
 LOOP	JSR RAND				; assign random color to mem[R6]
@@ -22,19 +27,33 @@ EXIT	ADD R6,R6,#1            ;
 
 SCREEN_START .FILL xC000
 SCREEN_END .FILL xFDFE
-RAND_SEED .FILL xFAEF
-RAND_MULT .FILL xD298
+
+RAND_SEED .FILL x0001
+RAND_MULT .FILL xF3B4
+RAND_INCR .FILL xA904
+RAND_LIM .FILL x7FFF		;(gets inverted)
 
 
 ;Functions ---------------
 
 RAND	; generates a random (ish) hex number from x0000 to xFFFF, stored in R0
-		LD R1,RAND_SEED
-		LD R2,RAND_MULT
-		JSR MULT
-		ST R0,RAND_SEED
+		LD R1,RAND_SEED		;
+		LD R2,RAND_MULT		; multiply seed with mult
+		JSR MULT			;
+
+		LD R1,RAND_INCR		; add increment
+		ADD R0,R0,R1		;
+
+		LD R1,RAND_LIM
+		ADD R1,R0,R1
+		BRn #1				;skip copy if R0 is bigger than limit
+		AND R0,R1,#-1		;copy R1 to R0
+
+		ST R0,RAND_SEED		; store
+
 		JSR EXIT
 
+		
 MULT	;multiplication - input R1, R2, output R0 - uses R0-R5
 		AND R0,R0,#0	; result
 		ADD R3,R0,#1	; bit test mask
