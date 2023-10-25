c08929d867ab619875fa1bf36fc9e49598f3910d
TheRad1 <landon.bakken@gmail.com>
2023-10-25 14:57:16 -0500

    stripped things down and added input

--- projects/p4/p4.asm
+++ projects/p4/p4.asm
@@ -23,7 +23,7 @@ WAIT	JSR GETKEYQ
 
 LOOP
 		LD R0,DELAY
-DELAYL	ADD R0,R0,#-1			; a delay so that the snake doesnt go zoom
+DELAYL	ADD R0,R0,#-1			; a delay so that the game is a little slower
 		BRp DELAYL
 
 		;key press
@@ -31,98 +31,31 @@ DELAYL	ADD R0,R0,#-1			; a delay so that the snake doesnt go zoom
 		BRz ENDKEY				;skip key finding if status is zero
 		JSR GETKEYQ
 
-		;R0 = pressed key, R1 = tested key, R2 = velocity
+		;R0 = pressed key, R1 = tested key, R2 = new velocity
 
-		LD R2,VEL
-		AND R4,R4,#0
-
+		AND R2,R2,#0
+		
 		LD R1,K_A
 		ADD R1,R0,R1			; A (left)
-		BRnp #1
-		ADD R4,R4,#-1
+		BRnp #2
+		ADD R2,R2,#-1
+		ST R2,PLAYER_VEL
 
 		LD R1,K_D
 		ADD R1,R0,R1			; D (right)
-		BRnp #1
-		ADD R4,R4,#1
-
-		ST R4,VEL
+		BRnp #2
+		ADD R2,R2,#1
+		ST R2,PLAYER_VEL
 
 ENDKEY
 		;past x pos = R0, past y pos = R1, new x pos = R3, new y pos = R4
 
-		LD R0,POS_X
-		LD R1,POS_Y
-		LD R5,VEL_X
-		LD R6,VEL_Y
-		AND R3,R0,#-1
-		AND R4,R1,#-1
-
-		; apply velocity
-		ADD R3,R3,R5
-		ADD R4,R4,R6
-		AND R0,R3,#-1
-		AND R1,R4,#-1
-		ST R3,POS_X
-		ST R4,POS_Y
-
-		; check bounds
-		AND R3,R3,#-1			;left
-		BRn DEATH
-
-		AND R4,R4,#-1			;top
-		BRn DEATH
-
-		LD R5,SCREEN_SIZEX_I	;right
-		ADD R3,R3,R5
-		BRz DEATH
-
-		LD R5,SCREEN_SIZEY_I	;bottom
-		ADD R4,R4,R5
-		BRz DEATH
-
-		JSR POINTADDR
-		LDR R0,R0,#0			;get color at to-be point
-		BRz #8					;skip checks if color is 0 INCREASE IF CHANGES TO THIS CHUNK IS MADE
-		LD R1,SNK_COL_I			; hit self
-		ADD R1,R0,R1
-		BRz DEATH
-		LD R1,FOOD_COL_I		; eat food
-		ADD R1,R0,R1
-		BRnp #2
-		JSR INCRSNAKE
-		JSR NEWFOOD
-
-		LD R0,POS_X
-		LD R1,POS_Y	
-		LD R2,SNK_COL			;print new pos
-		JSR POINT
-
-		LD R3,SNK_STR			;get last segment point and load into (R0,R1)
-		LD R1,SNK_OFF			
-		ADD R3,R3,R1
-		LDR R0,R3,#0			
-		LDR R1,R3,#1
-		LD R2,BGD_COL			;clear it
-		JSR POINT
-
-		LD R3,SNK_STR			
-		LD R1,SNK_OFF			
-		ADD R3,R3,R1
-		LD R0,POS_X				;overwrite with current pos
-		LD R1,POS_Y	
-		STR R0,R3,#0			
-		STR R1,R3,#1
-
-		LD R0,SNK_OFF
-		ADD R0,R0,#2
-		LD R1,SNK_LEN_I
-		ADD R1,R1,R0			;increment offset (reset if end of snake mem)
-		BRnp #1
-		AND R0,R0,#0
-		ST R0,SNK_OFF
+		
 
 		JSR LOOP				;loop
+
+;game functions --------------------
+
 
 ; data ----------------
 
@@ -161,62 +94,11 @@ BGD_COL_I .FILL x0000
 K_A		.FILL xFF9F				;x0061		Inverted key codes
 K_D		.FILL xFF9C				;x0064
 
-DELAY	.FILL x5000				;the delay in the program so it doesnt zoom so fast
-
-;game functions
-DEATH	TRAP x25
-
-
-INCRSNAKE	;increments the snake size
-		LD R0,SNK_LEN
-		ADD R0,R0,#2
-		ST R0,SNK_LEN
-
-		NOT R0,R0
-		ADD R0,R0,#1
-		ST R0,SNK_LEN_I
-		
-		RET
-
-NEWFOOD 
-		ST R7,SBSTORE			;store return
-
-		JSR RAND
-		LD R1,RAND_MASK
-		AND R0,R0,R1
-		ADD R0,R0,#10
-		ST R0,FOOD_X			;get new food pos
-		JSR RAND
-		LD R1,RAND_MASK
-		AND R0,R0,R1
-		ADD R0,R0,#10
-		ST R0,FOOD_Y
-		
-		LD R0,FOOD_X			
-		NOT R0,R0				;preprocess negative food position
-		ADD R0,R0,#1			
-		ST R0,FOOD_X_I			
-		LD R0,FOOD_Y			
-		NOT R0,R0				
-		ADD R0,R0,#1			
-		ST R0,FOOD_Y_I		
-		
-		LD R0,FOOD_X
-		LD R1,FOOD_Y
-		JSR POINTADDR
-		LD R7,SBSTORE
-		LDR R0,R0,#0			;check if new food position is on somethning else
-		BRnp NEWFOOD
-
-		LD R0,FOOD_X
-		LD R1,FOOD_Y			;print food
-		LD R2,FOOD_COL 
-		JSR POINT
-
-		LD R7,SBSTORE			;return
-		RET
+DELAY	.FILL x5000				;the delay in the program so it doesnt go so fast
 
 ;util functions --------------- Generally uses R0-R5
+
+DEATH	TRAP x25
 
 GETKEYW	;get key wait - in R0
 		LDI R0,KB_STE		; wait for a keystroke
@@ -291,48 +173,6 @@ POINT3						;
 		STR R2,R0,#0		; set color (R2) at mem[R0]
 
 		RET
-
-
-
-RAND	; generates a random (ish) hex number from x0000 to x7FFF, stored in R0
-		LD R0,RAND_SEED
-		ADD R0,R0,R0		; mult seed by 4
-
-		LD R1,RAND_INCR		; add increment
-		ADD R0,R0,R1		;
-
-		LD R1,RAND_LIM		; 
-		ADD R1,R0,R1		; wrap if bigger than RAND_LIM
-		BRn #1				;
-		AND R0,R1,#-1		;
-
-		ADD R0,R0,R0		;
-
-		LD R1,RAND_INCR		; add increment
-		ADD R0,R0,R1		;
-
-		LD R1,RAND_LIM		; 
-		ADD R1,R0,R1		; wrap if bigger than RAND_LIM
-		BRn #1				;
-		AND R0,R1,#-1		;
-
-		ADD R0,R0,R0		; 
-
-		LD R1,RAND_INCR		; add increment
-		ADD R0,R0,R1		;
-
-		LD R1,RAND_LIM		; 
-		ADD R1,R0,R1		; wrap if bigger than RAND_LIM
-		BRn #1				;
-		AND R0,R1,#-1		;
-
-		ST R0,RAND_SEED		; store seed
-
-		RET
-
-
-
-
 
 
 		.end				; KEEP AT THE END - errors from me being dumb and forgetting so far = 3
